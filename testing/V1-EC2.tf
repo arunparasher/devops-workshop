22d37af00c99acd18d0b9ee7e210ab8772b3c6bd
Arun Vanam <arun.vanam@outlook.com>
2025-06-07 15:05:25 +0530

    added v2

--- testing/V1-EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "demo" {
-  ami           = "ami-02457590d33d576c3"
-  instance_type = "t2.micro"
-  key_name = "terra"
-}
--- testing/V2-EC2.tf
+++ testing/V2-EC2.tf
@@ -5,11 +5,11 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami           = "ami-02457590d33d576c3"
   instance_type = "t2.micro"
-key_name = "terra"
-  security_groups = [aws_security_group.demo-sg.id]
+  key_name      = "terra"
+  security_groups = [aws_security_group.demo-sg.name]
 }
 
-resource "aws_security_group" "demo-sg {
+resource "aws_security_group" "demo-sg" {  # Fixed closing quote
   name        = "demo-sg"
   description = "SSH and HTTP access"
   tags = {
@@ -18,15 +18,15 @@ resource "aws_security_group" "demo-sg {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.demo-sg.id  # Corrected reference
+  cidr_ipv4         = "0.0.0.0/0"  # Fixed syntax
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.demo-sg.id  # Corrected reference
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1"
 }
